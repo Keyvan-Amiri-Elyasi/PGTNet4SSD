bc7f2e0de6ea8b59bc098fcc6c5e1aaf4834bbc2
keyvan-amiri <kamiriel@uni-mannheim.de>
2024-12-13 13:20:49 +0100

    update conversion to graphs

---
+++ CONVERT - SSD.sh
@@ -0,0 +1,19 @@
+export DIRECTORY=conversion_configs
+export CONFIG=helpdesk.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+export CONFIG=sepsis.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+export CONFIG=bpic15m1.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+export CONFIG=bpic15m2.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+export CONFIG=bpic15m3.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+export CONFIG=bpic15m4.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+export CONFIG=bpic15m5.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+export CONFIG=bpic12.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+export CONFIG=hospital.yaml
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
--- CONVERT.sh
+++ CONVERT.sh
@@ -1,19 +1,19 @@
 export DIRECTORY=conversion_configs
 export CONFIG=helpdesk.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true 
 export CONFIG=sepsis.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true
 export CONFIG=bpic15m1.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true
 export CONFIG=bpic15m2.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true
 export CONFIG=bpic15m3.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true
 export CONFIG=bpic15m4.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true
 export CONFIG=bpic15m5.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true
 export CONFIG=bpic12.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true
 export CONFIG=hospital.yaml
-python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true --ssd
+python GTconvertor.py ${DIRECTORY} ${CONFIG} --overwrite true
